6da6b08e8bbce24491c1d7b2dadd4cf65e93231d
Natalie Cooper <ncooper@tcd.ie>
2019-08-14 19:11:16 +0100

    Add more manipulations

--- explore.R
+++ explore.R
@@ -9,10 +9,11 @@ zz <-
   filter(institutionCode != "NHMUK") %>%
   filter(!is.na(genus)) %>%
   filter(!is.na(species)) %>%
+  filter(!is.na(locality) | !is.na(decimalLatitude)) %>%
   filter(basisOfRecord != "HUMAN_OBSERVATION") %>%
-  filter(is.na(decimalLatitude)) %>%
-  filter(!is.na(locality)) %>%
-  select(scientificName, locality, year, catalogNumber) %>%
+  unite(RegistrationNumber, c(institutionCode, catalogNumber)) %>%
+  select(RegistrationNumber, species, countryCode, locality, 
+         decimalLatitude, decimalLongitude, year, coordinateUncertaintyInMeters, issue) %>%
   distinct()
 
 # Additional 294 specimens BUT many zoo species
